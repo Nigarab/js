c94d80d480141764d24754ec4bea6ad4ed1eab41
Nigarab <127416315+Nigarab@users.noreply.github.com>
2023-04-19 22:23:35 +0400

    Update task3.js

--- js/task3/task3.js
+++ js/task3/task3.js
@@ -1,18 +1,30 @@
 function product() {  
     const productName = prompt("Enter product name");  
-    const unitePrice =  prompt("Enter unite price");  
-    const quantity =  prompt("Enter quantity"); 
-    const participates=  prompt("Participates in the promotion or not? ");  
+    const unitePrice =  (prompt("Enter unite price"));  
+    const quantity =  (prompt("Enter quantity")); 
+    const participates=  (prompt("Participates in the promotion or not? ")); 
+    const amount = unitePrice * 0.75
+    if (quantity>= 5 && participates == "yes" ) {
+      console.log(amount)
+      return amount
+      
+      
+    }
     const discount  = unitePrice * 0.9 
+    
     if (  quantity>= 5) { 
        console.log(discount); 
+       return discount
     }  
-     const discount2 = unitePrice * 0.85 
+    const discount2 = unitePrice * 0.85 
     if ( participates == "yes") { 
       console.log(discount2); 
+      return discount2
        
     }else if ( participates == "no"){ 
-      console.log(unitePrice) 
+      console.log(unitePrice) ;
+      return unitePrice
     } 
+    
   }  
  product();  
