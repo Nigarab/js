b99ffa0c878cf89c65547f9cd979f4f8aa87cbc1
Nigarab <127416315+Nigarab@users.noreply.github.com>
2023-04-20 21:22:36 +0400

    Update task5.js

--- js/task5/task5.js
+++ js/task5/task5.js
@@ -1,24 +1,26 @@
-function numbers() {
-  const a = prompt("Enter first number: ");
-  const b = prompt("Enter second number: ");
-  const c = prompt("Enter third number: ");
-  
-  if(a >= b && a >= c) {
-      if (b> c) {
-          console.log(a  , b , c )
-      }else{
-         console.log(a , c , b)
-      }
-  }
-  else if (b >= a && b >= c) {
-      if (a>c) {
-          console.log(b, a ,c)
-      }else{
-          console.log(b, c ,a)
-      }
-  }
-  else {
-      console.log(c , b , a)
-  }
-  }
-  numbers();
+document.getElementById("bottom").addEventListener("click", function () { 
+    event.preventDefault(); 
+ 
+        let a = document.getElementById("first").value 
+        let b = document.getElementById("second").value 
+        const c = document.getElementById("third").value 
+         
+        if(a >= b && a >= c) { 
+            if (b> c) { 
+                console.log(a  , b , c ) 
+            }else{ 
+               console.log(a , c , b) 
+            } 
+        } 
+        else if (b >= a && b >= c) { 
+            if (a>c) { 
+                console.log(b, a ,c) 
+            }else{ 
+                console.log(b, c ,a) 
+            } 
+        } 
+        else { 
+            console.log(c , b , a) 
+        } 
+             
+    })
