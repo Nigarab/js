b19fcc776b101c665eb75eef601208b1e7ed938c
Nigarab <127416315+Nigarab@users.noreply.github.com>
2023-04-20 21:19:32 +0400

    Update task3.js

--- js/task3/task3.js
+++ js/task3/task3.js
@@ -1,25 +1,24 @@
-function product() {  
-  const productName = prompt("Enter product name");  
-  const unitePrice =  (prompt("Enter unite price"));  
-  const quantity =  (prompt("Enter quantity")); 
-  const participates =  (prompt("Participates in the promotion or not? ")); 
-  const amount = unitePrice * 0.75
-  if (quantity >= 5 && participates.toLowerCase() == "yes") {
-    console.log(amount)
-    return amount
-  }
-  const discount = unitePrice * 0.9 
-  if (quantity >= 5) { 
-     console.log(discount); 
-     return discount
-  }  
-  const discount2 = unitePrice * 0.85 
-  if (participates.toLowerCase() == "yes") { 
-    console.log(discount2); 
-    return discount2       
-  } else if (participates.toLowerCase() == "no"){ 
-    console.log(unitePrice);
-    return unitePrice
-  } 
-}  
-product();  
+document.getElementById("bottom").addEventListener("click", function () { 
+  event.preventDefault(); 
+   let productName = document.getElementById("product").value 
+   let unitePrice= document.getElementById("prize").value 
+   let quantity =document.getElementById("quantity").value 
+   let participates =document.getElementById("promotion").value 
+   const amount = unitePrice * 0.75 
+   if (quantity >= 5 && participates == "yes") { 
+      return console.log(amount) 
+   } 
+   const discount = unitePrice * 0.9  
+   if (quantity >= 5) {  
+      return console.log(discount);  
+       
+   }   
+   const discount2 = unitePrice * 0.85  
+   if (participates == "yes") {  
+      return  console.log(discount2);  
+            
+   } else if (participates == "no"){  
+      return console.log(unitePrice); 
+       
+   }  
+  })
